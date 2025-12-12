81aa1c580c9f5a1e61eabb3de01425d6a53844cd
hentati salha <hentati.salha@iit.ens.tn>
2025-12-12 17:32:15 +0100

    Speaker

---
+++ EventWave/Services/SpeakerService.cs
@@ -0,0 +1,40 @@
+﻿using EventWave.Models;
+using EventWave.Repositories;
+
+namespace EventWave.Services
+{
+    public class SpeakerService : ISpeakerService
+    {
+        private readonly ISpeakerRepository speakerRepository;
+
+        public SpeakerService(ISpeakerRepository speakerRepository)
+        {
+            this.speakerRepository = speakerRepository;
+        }
+
+        public async Task<Speaker> CreateSpeakerAsync(Speaker speaker)
+        {
+            return await speakerRepository.AddAsync(speaker);
+        }
+
+        public async Task<List<Speaker>> GetAllSpeakersAsync()
+        {
+            return await speakerRepository.GetAllAsync();
+        }
+
+        public async Task<Speaker> GetSpeakerByIdAsync(int id)
+        {
+            return await speakerRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Speaker> UpdateSpeakerAsync(Speaker speaker)
+        {
+            return await speakerRepository.UpdateAsync(speaker);
+        }
+
+        public async Task<bool> DeleteSpeakerAsync(int id)
+        {
+            return await speakerRepository.DeleteAsync(id);
+        }
+    }
+}
